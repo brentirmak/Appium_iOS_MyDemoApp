9978f9548203ad33e304c9dc9b59ca83d71e69fc
Brent Irmak <brent.irmak@gmail.com>
2026-06-02 11:25:36 -0700

    Add pytest checks

--- First_Test.py
+++ First_Test.py
@@ -1,7 +1,8 @@
 # This sample code supports Appium Python client >=2.3.0
-# pip install Appium-Python-Client
-# Then you can paste this into a file and simply run with Python
+# pip install Appium-Python-Client pytest
+# Run with: pytest test_demo_app.py -v
 
+import pytest
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
@@ -12,46 +13,61 @@ from selenium.webdriver.common.actions import interaction
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.pointer_input import PointerInput
 
-options = AppiumOptions()
-options.load_capabilities({
-	"platformName": "ios",
-	"appium:automationName": "xcuitest",
-	"appium:deviceName": "iPhone 17 Pro",
-	"appium:platformVersion": "26.5",
-	"appium:udid": "89681D14-673F-445A-80AA-351832829080",
-	"appium:noReset": True,
-	"appium:fullReset": False,
-	"appium:printPageSourceOnFindFailure": False,
-	"appium:app": "/Users/seiwa/Desktop/my-demo-app-ios-main/build/Build/Products/Debug-iphonesimulator/My Demo App.app",
-	"appium:waitForQuiescence": False,
-	"appium:includeSafariInWebviews": True,
-	"appium:newCommandTimeout": 3600,
-	"appium:connectHardwareKeyboard": True
-})
 
-driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+APPIUM_SERVER_URL = "http://127.0.0.1:4723"
 
-try:
-	# //XCUIElementTypeImage[@name="Menu Icons"]
-	driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="Menu Icons"]').click()
+CAPABILITIES = {
+    "platformName": "ios",
+    "appium:automationName": "xcuitest",
+    "appium:deviceName": "iPhone 17 Pro",
+    "appium:platformVersion": "26.5",
+    "appium:udid": "89681D14-673F-445A-80AA-351832829080",
+    "appium:noReset": True,
+    "appium:fullReset": False,
+    "appium:printPageSourceOnFindFailure": False,
+    "appium:app": "/Users/seiwa/Desktop/my-demo-app-ios-main/build/Build/Products/Debug-iphonesimulator/My Demo App.app",
+    "appium:waitForQuiescence": False,
+    "appium:includeSafariInWebviews": True,
+    "appium:newCommandTimeout": 3600,
+    "appium:connectHardwareKeyboard": True,
+}
 
-	# Confirm that there's a Login link
-	element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="LogOut-menu-item"]')
-	assert element is not None, "LogOut menu item was not found"
 
-	# Click on Webview menu item
-	driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Webview"]').click()
+@pytest.fixture(scope="class")
+def driver():
+    """Set up and tear down the Appium driver for the test session."""
+    options = AppiumOptions()
+    options.load_capabilities(CAPABILITIES)
+    _driver = webdriver.Remote(APPIUM_SERVER_URL, options=options)
+    yield _driver
+    _driver.quit()
 
-	# Confirm Enter an https URL item label is found
-	element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Enter an https URL"]')
-	assert element is not None, "Enter an https URL item was not found"
 
-	# Click on the Back button
-	element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="BackButton Icons"]').click()
+class TestDemoApp:
 
-	# Confirm that My DemoApp logo is found
-	element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="AppTitle Icons"]')
-	assert element is not None, "My DemoApp logo was not found"
-	
-finally:
-	driver.quit()
+    def test_open_menu(self, driver):
+        """Tap the menu icon to open the side/hamburger menu."""
+        driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="Menu Icons"]').click()
+
+    def test_logout_menu_item_visible(self, driver):
+        """Confirm the LogOut menu item is visible after opening the menu."""
+        element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="LogOut-menu-item"]')
+        assert element is not None, "LogOut menu item was not found"
+
+    def test_navigate_to_webview(self, driver):
+        """Tap the Webview menu item to navigate to the Webview screen."""
+        driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Webview"]').click()
+
+    def test_webview_url_input_visible(self, driver):
+        """Confirm the URL input label is present on the Webview screen."""
+        element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="Enter an https URL"]')
+        assert element is not None, "Enter an https URL item was not found"
+
+    def test_navigate_back(self, driver):
+        """Tap the Back button to return to the home screen."""
+        driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="BackButton Icons"]').click()
+
+    def test_home_logo_visible(self, driver):
+        """Confirm the My DemoApp logo is visible on the home screen."""
+        element = driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeImage[@name="AppTitle Icons"]')
+        assert element is not None, "My DemoApp logo was not found"
